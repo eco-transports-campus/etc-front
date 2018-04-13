468a48d8a68368062e6f72bda7a080a68debf92e
Léo Donny <leo.donny2@gmail.com>
2018-04-13 11:58:15 +0200

    (front-archi) eng -> fr  in menu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
         { title: 'Demander un trajet', icon: 'transfer_within_a_station', link: 'travel/request', withDivider: true },
         { title: 'Statistiques', icon: 'show_chart', link: 'stats', withDivider: true },
         { title: 'Reporter un bug', icon: 'bug_report', link: 'bug', withDivider: false },
-        { title: 'About', icon: 'account_circle', link: 'about', withDivider: false }
+        { title: 'À propos', icon: 'account_circle', link: 'about', withDivider: false }
     ]
 };
