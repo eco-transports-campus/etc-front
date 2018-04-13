02f2e606120efa1a2ed64f60aa5b111ec76f7912
Léo Donny <leo.donny2@gmail.com>
2018-04-13 17:08:01 +0200

    (front-archi) move environments files into app folder (logic)

--- src/app/modules/core/components/layout/layout.component.ts
+++ src/app/modules/core/components/layout/layout.component.ts
@@ -1,6 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { environment } from '../../../../../environments/environment';
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-layout',
