e4713c233a4d00c3980aac6d1f4a3d37177ab8e2
Clément Garin <clement.garin@outlook.fr>
2018-05-01 19:54:55 +0200

    #13 - Adding notifications to the main layout

--- src/app/modules/core/components/layout/layout.component.ts
+++ src/app/modules/core/components/layout/layout.component.ts
@@ -1,3 +1,5 @@
+import { Notification } from './../../../notification/models/notifications';
+import { NotificationService } from './../../../notification/services/notification.service';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
@@ -9,14 +11,38 @@ import { environment } from '../../../../../environments/environment';
 })
 export class LayoutComponent implements OnDestroy {
     mobileQuery: MediaQueryList;
+    notifications: Notification[] = [];
+    unreadNotifications = 0;
 
     private _mobileQueryListener: () => void;
     public env = environment;
 
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    constructor(private notif: NotificationService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
         this.mobileQuery = media.matchMedia('(max-width: 600px)');
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
+        this.retrievedNotification();
+        this.listenNotification();
+    }
+
+    retrievedNotification(): void {
+      // TODO: Use real user id
+      this.notif.getNotificationsForUser(1).subscribe(
+        data => {
+          this.notifications = data;
+          this.unreadNotifications = this.notifications.filter(notification => notification.unread).length;
+        }
+      );
+    }
+
+    listenNotification(): void {
+      // TODO: Use real user id
+      this.notif.listenNewNotificationForUser(1).subscribe(
+        data => {
+          this.notifications.push(data);
+          this.unreadNotifications++;
+        }
+      );
     }
 
     ngOnDestroy(): void {
