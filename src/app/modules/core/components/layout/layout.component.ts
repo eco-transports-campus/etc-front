41204377c2df793f1882526cf4bcec125ba0386d
Clément Garin <clement.garin@outlook.fr>
2018-05-02 10:58:42 +0200

    #13 - Adding notification page

--- src/app/modules/core/components/layout/layout.component.ts
+++ src/app/modules/core/components/layout/layout.component.ts
@@ -1,15 +1,19 @@
 import { Notification } from './../../../notification/models/notifications';
 import { NotificationService } from './../../../notification/services/notification.service';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild, AfterContentInit } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-layout',
     templateUrl: 'layout.component.html',
     styleUrls: ['layout.component.scss'],
 })
-export class LayoutComponent implements OnDestroy {
+export class LayoutComponent implements OnDestroy, AfterContentInit {
+
+    @ViewChild('notificationNav') notificationNav: MatSidenav;
+
     mobileQuery: MediaQueryList;
     notifications: Notification[] = [];
     unreadNotifications = 0;
@@ -23,6 +27,15 @@ export class LayoutComponent implements OnDestroy {
         this.mobileQuery.addListener(this._mobileQueryListener);
         this.retrievedNotification();
         this.listenNotification();
+    }
+
+    openNotification(): void {
+      this.notificationNav.toggle();
+    }
+
+    setNotificationRead(): void {
+      this.notifications.forEach(item => item.unread = false);
+      this.unreadNotifications = 0;
     }
 
     retrievedNotification(): void {
@@ -43,6 +56,10 @@ export class LayoutComponent implements OnDestroy {
           this.unreadNotifications++;
         }
       );
+    }
+
+    ngAfterContentInit(): void {
+      this.notificationNav.onClose.subscribe(data => this.setNotificationRead());
     }
 
     ngOnDestroy(): void {
--- src/app/modules/notification/components/list/notifications-list.component.ts
+++ src/app/modules/notification/components/list/notifications-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NotificationService } from '../../services/notification.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Notification } from '../../models/notifications';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications-list.component.html',
   styleUrls: ['./notifications-list.component.scss']
 })
-export class NotificationsListComponent implements OnInit {
+export class NotificationsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private notificationSub$: Subscription;
+  notifications: Notification[] = [];
+  unreadNotifications = 0;
+
+  constructor(private notif: NotificationService) { }
 
   ngOnInit() {
+    // TODO: use real user ID
+    this.notif.getNotificationsForUser(1).subscribe(
+      notifications => {
+        notifications.forEach(item => {
+          this.notifications.push(item);
+          this.unreadNotifications = item.unread ? this.unreadNotifications++ : this.unreadNotifications;
+        });
+      }
+    );
+
+     // TODO: use real user ID
+    this.notificationSub$ = this.notif.listenNewNotificationForUser(1).subscribe(
+      notification => {
+        this.notifications.push(notification);
+        this.unreadNotifications = notification.unread ? this.unreadNotifications++ : this.unreadNotifications;
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.notificationSub$.unsubscribe();
+  }
 }
