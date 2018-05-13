a819c3bf51e34e4ea2d1b09b82f2b3521623bfff
ImenAmmar <imen.ammar29@gmail.com>
2018-05-14 01:24:29 +0200

    add about page

--- src/app/modules/core/components/home/home.component.ts
+++ src/app/modules/core/components/home/home.component.ts
@@ -9,7 +9,13 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  searchroot(){
+
+    console.log("Trouver un trajet !");
   }
 
+
+  ngOnInit() {
+  }
+  
 }
